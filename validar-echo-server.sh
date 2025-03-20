536e3ae483de601374f2b58d1393402f04a6d06e
Val <vcorrea@fi.uba.ar>
2025-03-20 20:11:23 -0300

    refactor timeout

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -7,7 +7,7 @@ SERVER_PORT=$(awk -F " = " '/SERVER_PORT/ {print $2}' ./server/config.ini)
 SERVER_IP=$(awk -F " = " '/SERVER_IP/ {print $2}' ./server/config.ini)
 
 # Uso netcat para enviar mensaje al server y guardo respuesta
-RESPONSE=$(echo "$MESSAGE" | nc $SERVER_IP $SERVER_PORT)
+RESPONSE=$(echo "$MESSAGE" | nc -w 2 $SERVER_IP $SERVER_PORT)
 
 # Valido resultado
 if [ "$RESPONSE" == "$MESSAGE" ]; then
