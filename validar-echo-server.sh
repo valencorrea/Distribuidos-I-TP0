b3fa07cea71a4e7c96484ae59e76207c9dedc43a
Val <vcorrea@fi.uba.ar>
2025-03-20 20:24:25 -0300

    rm variable for server ip

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -4,10 +4,9 @@ MESSAGE="Hello, server!"
 
 # Obtengo el puerto del server del archivo de config
 SERVER_PORT=$(awk -F " = " '/SERVER_PORT/ {print $2}' ./server/config.ini)
-SERVER_IP=$(awk -F " = " '/SERVER_IP/ {print $2}' ./server/config.ini)
 
 # Uso netcat para enviar mensaje al server y guardo respuesta
-RESPONSE=$(echo "$MESSAGE" | nc -w 2 $SERVER_IP $SERVER_PORT)
+RESPONSE=$(echo "$MESSAGE" | nc -w 2 server $SERVER_PORT)
 
 # Valido resultado
 if [ "$RESPONSE" == "$MESSAGE" ]; then
