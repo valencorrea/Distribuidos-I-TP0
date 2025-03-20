f6f8603d101319354c8f8c2e9d1784f144897059
Val <vcorrea@fi.uba.ar>
2025-03-20 20:26:36 -0300

    bash error

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -9,7 +9,7 @@ SERVER_PORT=$(awk -F " = " '/SERVER_PORT/ {print $2}' ./server/config.ini)
 RESPONSE=$(echo "$MESSAGE" | nc -w 2 server $SERVER_PORT)
 
 # Valido resultado
-if [ "$RESPONSE" == "$MESSAGE" ]; then
+if [ "$RESPONSE" = "$MESSAGE" ]; then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"
