7c96f30e34f0445f1e44a5bc5a1391bee4db691c
Val <vcorrea@fi.uba.ar>
2025-03-22 11:34:30 -0300

    add defer

--- client/common/client.go
+++ client/common/client.go
@@ -67,7 +67,7 @@ func (c *Client) StartClientLoop() {
 			msgID,
 		)
 		msg, err := bufio.NewReader(c.conn).ReadString('\n')
-		c.conn.Close()
+		defer c.conn.Close()
 
 		if err != nil {
 			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
