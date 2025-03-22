2295e844b8bc79236438e57ce56180579dbf7fe4
Val <vcorrea@fi.uba.ar>
2025-03-22 01:19:18 -0300

    refactor client close

--- server/common/server.py
+++ server/common/server.py
@@ -30,16 +30,17 @@ class Server:
 
         while not self.kill_now:
             client_sock = self.__accept_new_connection()
-            try:
-                self.__handle_client_connection(client_sock)
-            finally:
-                self.exit_gracefully(client_sock)
+            self.__handle_client_connection(client_sock)
 
-    def exit_gracefully(self, client_sock):
+            logging.info("Closing client socket...")
+            client_sock.close()
+
+        self.exit_gracefully()
+
+    def exit_gracefully(self):
         logging.info("Closing server socket...")
         self._server_socket.close()
-        logging.info("Closing client socket...")
-        client_sock.close()
+
 
     def __handle_client_connection(self, client_sock):
         """
