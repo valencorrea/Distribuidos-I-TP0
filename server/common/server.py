8a9f34f42424845ebe4250f77134b6c53d865e6d
Val <vcorrea@fi.uba.ar>
2025-03-21 21:59:23 -0300

    refactor server

--- server/common/server.py
+++ server/common/server.py
@@ -1,13 +1,20 @@
 import socket
 import logging
+import signal
 
 
 class Server:
+    kill_now = False
+
     def __init__(self, port, listen_backlog):
         # Initialize server socket
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        signal.signal(signal.SIGTERM, self.do_kill_now)
+
+    def do_kill_now(self, signum, frame):
+        self.kill_now = True
 
     def run(self):
         """
@@ -20,9 +27,16 @@ class Server:
 
         # TODO: Modify this program to handle signal to graceful shutdown
         # the server
-        while True:
+
+        while not self.kill_now:
             client_sock = self.__accept_new_connection()
             self.__handle_client_connection(client_sock)
+
+        self.exit_gracefully()
+
+    def exit_gracefully(self):
+        logging.info("Closing server socket...")
+        self._server_socket.close()
 
     def __handle_client_connection(self, client_sock):
         """
