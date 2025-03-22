65a1978b007da2c362bdc08ec0a978c357a1897d
Val <vcorrea@fi.uba.ar>
2025-03-22 01:53:01 -0300

    add err cond client

--- client/common/client.go
+++ client/common/client.go
@@ -45,6 +45,7 @@ func (c *Client) createClientSocket() error {
 			c.config.ID,
 			err,
 		)
+        return err
 	}
 	c.conn = conn
 	return nil
@@ -56,7 +57,12 @@ func (c *Client) StartClientLoop() {
 	// Messages if the message amount threshold has not been surpassed
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
 		// Create the connection the server in every loop iteration. Send an
-		c.createClientSocket()
+		result = c.createClientSocket()
+        if result != nil {
+            log.Errorf("Error creating socket")
+            c.conn.Close()
+            return
+        }
 
 		// TODO: Modify the send to avoid short-write
 		fmt.Fprintf(
