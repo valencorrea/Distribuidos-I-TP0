28d04381e00751ef4ce93e3e705d405745bf9626
Val <vcorrea@fi.uba.ar>
2025-03-25 20:59:59 -0300

    change to break

--- client/common/client.go
+++ client/common/client.go
@@ -73,8 +73,7 @@ func (c *Client) StartClientLoop() {
 		case <-signalChannel:
     		c.conn.Close()
             log.Infof("action: close_client_socket | result: success")
-            close(signalChannel)
-			return
+			break
 		default:
             // Create the connection the server in every loop iteration. Send an
             c.createClientSocket()
