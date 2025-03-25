ddd0c9c4d58b40c525764ed76929a18be7a131a1
Val <vcorrea@fi.uba.ar>
2025-03-25 20:44:52 -0300

    change signal select

--- client/common/client.go
+++ client/common/client.go
@@ -70,7 +70,7 @@ func (c *Client) StartClientLoop() {
 	// Messages if the message amount threshold has not been surpassed
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
 		select {
-		case <-signalChannel:
+		case <-exitChannel:
 			return
 		default:
             // Create the connection the server in every loop iteration. Send an
