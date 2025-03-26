8590924d6cfc3351248a5e9058d027e8dd698ca6
Val <vcorrea@fi.uba.ar>
2025-03-26 07:57:16 -0300

    add log

--- client/common/protocol.go
+++ client/common/protocol.go
@@ -6,7 +6,9 @@ import (
 )
 
 func formatBetMessage(c *Client) string {
-	return fmt.Sprintf("B;%s;%s;%s;%s;%s;%s\n", c.config.ID, c.config.Name, c.config.Surname, c.config.IDNumber, c.config.DateOfBirth, c.config.BetNumber)
+	message := fmt.Sprintf("B;%s;%s;%s;%s;%s;%s\n", c.config.ID, c.config.Name, c.config.Surname, c.config.IDNumber, c.config.DateOfBirth, c.config.BetNumber)
+	log.Infof("action: sending_message | result: success | message: %v", message)
+	return message
 }
 
 func writeBetMessage(c *Client, message string) error {
