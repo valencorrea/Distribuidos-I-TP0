45b04c350d0c90aa4015b2b607fafe916737abb2
Val <vcorrea@fi.uba.ar>
2025-03-26 02:02:06 -0300

    remove loop

--- client/common/client.go
+++ client/common/client.go
@@ -63,56 +63,54 @@ func (c *Client) StartClientLoop() {
 
 	// There is an autoincremental msgID to identify every message sent
 	// Messages if the message amount threshold has not been surpassed
-	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
-		select {
-		case <-signalChannel:
-			if c.conn != nil {
-				c.conn.Close()
-				log.Infof("action: close_client_socket | result: success")
-			}
-			return
-		default:
-			// Create the connection the server in every loop iteration. Send an
-			err := c.createClientSocket()
-			if err != nil {
-				log.Errorf("action: create_client_socket | result: fail | client_id: %v | error: %v",
-					c.config.ID,
-					err,
-				)
-				return
-			}
-
-			err = c.doBet()
-			if err != nil {
-				log.Errorf("action: do_bet | result: fail | client_id: %v | error: %v",
-					c.config.ID,
-					err,
-				)
-				c.conn.Close()
-				return
-			}
-
-			log.Infof("action: do_bet | result: success | client_id: %v",
-				c.config.ID)
-
-			err = c.receiveBetResponse()
-			if err != nil {
-				log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-					c.config.ID,
-					err,
-				)
-				c.conn.Close()
-				return
-			}
-
+	select {
+	case <-signalChannel:
+		if c.conn != nil {
 			c.conn.Close()
-
-			log.Infof("action: apuesta_enviada | result: success | dni: %v | numero: %v",
-				c.config.IDNumber, c.config.BetNumber)
-
-			// Wait a time between sending one message and the next one
-			time.Sleep(c.config.LoopPeriod)
+			log.Infof("action: close_client_socket | result: success")
 		}
+		return
+	default:
+		// Create the connection the server in every loop iteration. Send an
+		err := c.createClientSocket()
+		if err != nil {
+			log.Errorf("action: create_client_socket | result: fail | client_id: %v | error: %v",
+				c.config.ID,
+				err,
+			)
+			return
+		}
+
+		err = c.doBet()
+		if err != nil {
+			log.Errorf("action: do_bet | result: fail | client_id: %v | error: %v",
+				c.config.ID,
+				err,
+			)
+			c.conn.Close()
+			return
+		}
+
+		log.Infof("action: do_bet | result: success | client_id: %v",
+			c.config.ID)
+
+		err = c.receiveBetResponse()
+		if err != nil {
+			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
+				c.config.ID,
+				err,
+			)
+			c.conn.Close()
+			return
+		}
+
+		c.conn.Close()
+
+		log.Infof("action: apuesta_enviada | result: success | dni: %v | numero: %v",
+			c.config.IDNumber, c.config.BetNumber)
+
+		// Wait a time between sending one message and the next one
+		time.Sleep(c.config.LoopPeriod)
 	}
 
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
