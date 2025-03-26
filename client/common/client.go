11f1fc9014463883c63fc87a3c8d1710731f0dce
Val <vcorrea@fi.uba.ar>
2025-03-25 22:03:08 -0300

    comment msgId

--- client/common/client.go
+++ client/common/client.go
@@ -68,7 +68,7 @@ func (c *Client) StartClientLoop() {
                 c.conn.Close()
                 log.Infof("action: close_client_socket | result: success")
             }
-            msgID = c.config.LoopAmount+1
+            //msgID = c.config.LoopAmount+1
 			return
 		default:
 			// Create the connection the server in every loop iteration. Send an
