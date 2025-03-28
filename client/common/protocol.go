5628fe06ebbb79f16c8bfb6a2656adaeb8f4143d
Val <vcorrea@fi.uba.ar>
2025-03-27 22:58:21 -0300

    as

--- client/common/protocol.go
+++ client/common/protocol.go
@@ -57,9 +57,6 @@ func decodeBetResponse(c *Client) error {
 		log.Infof("action: reading_bet_response | result: success | msg: %v", msg)
 	} else if msg[0] == 'W' {
 		documents := strings.Split(strings.TrimSuffix(msg, "\n"), ";")
-		//documents := strings.Split(msg, ";")
-		log.Infof("aDOCUMENTOS: %v", documents)
-		log.Infof("len documents: %v", len(documents))
 		log.Infof("action: consulta_ganadores | result: success | cant_ganadores: %v", len(documents)-1)
 	}
 	return nil
